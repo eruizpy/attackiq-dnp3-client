a7ccb3c2251a245e61aee18529f81c0f3f2eb459
jadamcrain <jadamcrain@gmail.com>
2016-07-06 10:58:25 -0700

    working program

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,9 +2,10 @@
 #include <asiodnp3/PrintingSOEHandler.h>
 #include <asiodnp3/ConsoleLogger.h>
 #include <asiodnp3/DefaultMasterApplication.h>
-#include <asiodnp3/PrintingCommandCallback.h>
 
 #include <opendnp3/LogLevels.h>
+
+#include <future>
 
 #include "SyncVar.h"
 
@@ -16,62 +17,84 @@ using namespace opendnp3;
 
 MasterStackConfig GetMasterConfig();
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
 
-	// Specify what log levels to use. NORMAL is warning and above
-	// You can add all the comms logging by uncommenting below
-	const uint32_t FILTERS = levels::NORMAL | levels::ALL_APP_COMMS;
+    // Specify what log levels to use. NORMAL is warning and above
+    // You can add all the comms logging by uncommenting below
+    const uint32_t FILTERS = levels::NORMAL | levels::ALL_APP_COMMS;
 
     // we'll use this synchronized variable to monitor the state of the channel
     SyncVar<ChannelState> channelstate(ChannelState::CLOSED);
 
-	// This is the main point of interaction with the stack
-	DNP3Manager manager(1, ConsoleLogger::Create());
+    // This is the main point of interaction with the stack
+    DNP3Manager manager(1, ConsoleLogger::Create());
 
-	// Connect via a TCPClient socket to a outstation
-	auto pChannel = manager.AddTCPClient("tcpclient", FILTERS, ChannelRetry::Default(), "127.0.0.1", "0.0.0.0", 20000);
+    // Connect via a TCPClient socket to a outstation
+    auto pChannel = manager.AddTCPClient("tcpclient", FILTERS, ChannelRetry::Default(), "127.0.0.1", "0.0.0.0", 20000);
 
-	// update the synchronized variable whenever the channel state changes
-	pChannel->AddStateListener([&channelstate](ChannelState state)
-	{
+    // update the synchronized variable whenever the channel state changes
+    pChannel->AddStateListener([&channelstate](ChannelState state) {
         channelstate.Set(state);
-	});
+    });
 
-	// Create a new master on a previously declared port, with a
-	// name, log level, command acceptor, and config info. This
-	// returns a thread-safe interface used for sending commands.
-	auto master = pChannel->AddMaster(
-	                  "master",										    // id for logging
-	                  PrintingSOEHandler::Instance(),					// callback for data processing
-	                  DefaultMasterApplication::Instance(),             // master application instance
-	                  GetMasterConfig()									// stack configuration
-	);
+    // Create a new master on a previously declared port, with a
+    // name, log level, command acceptor, and config info. This
+    // returns a thread-safe interface used for sending commands.
+    auto master = pChannel->AddMaster(
+            "master",                                         // id for logging
+            PrintingSOEHandler::Instance(),                   // callback for data processing
+            DefaultMasterApplication::Instance(),             // master application instance
+            GetMasterConfig()                                 // stack configuration
+    );
 
-	// Enable the master. This will start communications.
-	master->Enable();
+    // Enable the master. This will start communications.
+    master->Enable();
 
     // Wait for a connection for a period of time
-    if(!channelstate.WaitFor(ChannelState::OPEN, std::chrono::seconds(5)))
-    {
+    if (!channelstate.WaitFor(ChannelState::OPEN, std::chrono::seconds(5))) {
         std::cerr << "Unable to connect within timeout" << std::endl;
         return 1;
     }
 
-	//ControlRelayOutputBlock crob(ControlCode::LATCH_ON);
-	//master->SelectAndOperate(crob, 0, PrintingCommandCallback::Get());
+    std::promise<CommandPointResult> p;
+    auto callback = [&p](const ICommandTaskResult & results) -> void
+    {
+        CommandPointResult result(0,0, CommandPointState::INIT, CommandStatus::UNDEFINED);
+        results.ReadOnlyValue(result);
+        p.set_value(result);
+    };
 
-	return 0;
+    master->SelectAndOperate(ControlRelayOutputBlock(ControlCode::LATCH_ON), 0, callback);
+
+    // wait for the operation to complete
+    auto future = p.get_future();
+    future.wait();
+    auto result = future.get();
+
+    if(result.state != CommandPointState::SUCCESS)
+    {
+        std::cerr << "Command operation unsuccessful: " << CommandPointStateToString(result.state) << std::endl;
+        return 2;
+    }
+
+    if(result.status != CommandStatus::SUCCESS)
+    {
+        std::cerr << "Outstation reported failure: " << CommandStatusToString(result.status) << std::endl;
+        return 3;
+    }
+
+    return 0;
 }
 
-MasterStackConfig GetMasterConfig()
-{
+MasterStackConfig GetMasterConfig() {
+
     MasterStackConfig config;
 
+    config.master.ignoreRestartIIN = true;
     config.master.startupIntegrityClassMask = ClassField::None();
     config.master.unsolClassMask = ClassField::None();
     config.master.disableUnsolOnStartup = false;
-    
+
     // You can override the default link layer settings here
     // in this example we've changed the default link layer addressing
     config.link.LocalAddr = 1;
