d5a350c7a7c8b51707f9ce6e8252c9c4c6144a15
jadamcrain <jadamcrain@gmail.com>
2016-07-12 09:18:03 -0700

    typo

--- src/client/CommandLineOptions.cpp
+++ src/client/CommandLineOptions.cpp
@@ -4,7 +4,7 @@ CommandLineOptions::CommandLineOptions() :
         cmd("dnp3-master", ' ', "0.1"),
         hostPort("p", "port", "Port the client connects on", false, 20000, "tcp port"),
         hostAddress("a", "hostAddress", "IPv4 address of the remote host", false, "127.0.0.1", "ipv4 address"),
-	localAdapter("l", "localAdapter", "IPv4 address of the local ethernet adater", false, "0.0.0.0", "local adapter"),
+	localAdapter("l", "localAdapter", "IPv4 address of the local ethernet adapter", false, "0.0.0.0", "local adapter"),
         masterAddress("m", "masterAddress", "DNP3 address of the local master", false, 1, "master address"),
         outstationAddress("o", "outstationAddress", "DNP3 address of the remote outstation", false, 1024, "outstation address")
 {
